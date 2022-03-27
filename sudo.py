81736d0c5ad6546a9e571969ba9865f53841a96e
Mateusz <m.stepien1606@gmail.com>
2022-03-27 10:37:01 +0200

    changing random to backtrack but recurention still don't work

--- sudo.py
+++ sudo.py
@@ -1,72 +1,106 @@
 import random
 #walidacja w rzedzie
-
+"""
+zmiana 3 na zmienne
+napisanie backtrackingu
+zamiana sortowania na quicksort
+napisanie testow jednostkowych
+powtorka goscia prof
+dokonczenie rozdzialu goscia prof
+"""
 
 class Sudoku():
     def __init__(self):
-        pass
+        #nie lista tabelek zer tylko lista klasy Square która będzie miala domyślnie ustawionego taba z 0
+        self.sudo_tab = [Square() for x in range(9)]
+
+    def get_used_in_column(self, n \
+        ):
+        # n to macierz trzeba iterować po jej szerokości
+        list_used_set = []
+        for j in range(3):
+            used_set = set()
+            for i in [1,4,7]:
+                for k in range(3):
+                    used_set.add(self.sudo_tab[i].croot[k][j])
+            used_set=used_set.difference({0})
+            list_used_set.append(used_set)
+        return list_used_set
+    def get_used_in_row(self, n):
+        # n liczone od jeden z powodu sprawdzania podzielności w przypadku z 0 nie zadziała poprawnie
+        list_used_set = []
+        for j in range(3):
+            used_set = set()
+            for i in range(3*n, 3*n+3):
+                for k in range(3):
+                    used_set.add(self.sudo_tab[i].croot[j][k])
+            used_set=used_set.difference({0})
+            list_used_set.append(used_set)
+        return list_used_set
+
+#tutaj trójka musi byc zastąpiona wysokością prostokątów wewnętrznych
+        r = n//3
+        for i in range(3):
+            used_set.add(self.sudo_tab[r].croot[n][i])
+        used_set.difference({0})
+        return used_set
+    def print_row(self, num):
+        n = num * 3
+        for j in range(3):
+            print("|", end=' ')
+            for i in range(n-3, n):
+                for k in range(3):
+                    print(self.sudo_tab[i].croot[j][k], end=' ')
+                print("|", end=' ')
+            print()
+
+
+    def print_full_sudoku(self, separate_row=True):
+        if(separate_row):print("-"*25)
+        for i in range(1,4):
+            self.print_row(i)
+            if(separate_row):print("-"*25)
+    
+
+    def test_backtrack(self, sbu, i, used_here):
+
+        if i==9:
+            return True
+        numbers = [x for x in range(1, 10)]
+        numbers = set(numbers).difference(sbu[i][2])
+        if used_here:
+            s=set(used_here)
+            numbers = numbers.difference(s)
+        numbers = list(numbers)
+        if len(numbers)==0:
+            print("zwracam fałsz")
+            return False
+        
+        
+        for x in numbers:
+            self.sudo_tab[1].croot[sbu[i][0]][sbu[i][1]] = x
+            print(i, x)
+            i+=1
+            used_here.append(x)
+            return self.test_backtrack(sbu, i, used_here)
+
+
+
 
 
 class Square():
 
     def __init__(self, unr=[], unc=[]):
-        self.croot = self.full_num(use_num_row=unr, use_num_column=unc)
+        self.croot = [[0 for x in range(3)] for z in range(3)]
 
-    def full_num(self, use_num_row=[], use_num_column=[]):
-        croot = [[0 for x in range(3)] for z in range(3)]
-        y = 0
+    def create_unbound_place(self):
         numbers = [x for x in range(1, 10)]
-        if use_num_row or use_num_column:
-            li_union=self.create_list_union(use_num_row=use_num_row, use_num_column=use_num_column)
-            print(li_union)
-            print("_____________________________________")
-            li_union=self.sort_by_union(li_union)
-            print("________________________")
-            print(li_union[0][0],li_union[0][1])
-            print("________________________")
-            i=0
-            used_here=set()
-            while(i<len(li_union)):
-                numbers = [x for x in range(1, 10)]
-                numbers = set(numbers).difference(li_union[i][2])
-                numbers = numbers.difference(used_here)
-                numbers = list(numbers)
+        for i in range(3):
+            for j in range(3):
                 r = random.randint(0, len(numbers)-1)
-                used_here.add(numbers[r])
-                print(i)
-                croot[li_union[i][0]][li_union[i][1]]=numbers[r]
-                i+=1
+                a = numbers.pop(r)
+                self.croot[i][j] = a
 
-            
-        else:
-            while(y < 3):
-
-                i = 0
-
-                while(i < 3):
-
-                    r = random.randint(0, len(numbers)-1)
-                    a = numbers.pop(r)
-                    croot[y][i] = a
-                    i += 1
-
-                y += 1
-        return croot
-
-    def print_croot(self):
-        # tutaj by wypadalo ladniej wypisywac
-        for q in self.croot:
-            print(q)
-
-    def give_row(self, ind):
-        rw = self.croot[ind]
-        return rw
-
-    def give_col(self, ind):
-        cl = []
-        for x in self.croot:
-            cl.append(x[ind])
-        return cl
 
     def create_list_union(self, use_num_row=[], use_num_column=[]):
         li = []
@@ -88,18 +122,14 @@ class Square():
         #inicjalizacja zmiennych odpowiedzialnych za pętle
         n = len(union_list)
         i=0
-        
- 
         while(i<=n-1):
             #zadeklarowanie jako mini elementu o inteksie w którym chce wstawić najmniejszą wartość
             mini = union_list[i]
-            print(mini)
             indMin = i
             j=i+1
             while(j<n):
                 #znajduje najmniejszy element z pozostałej listy
-                print(i, j)
-                if (len(union_list[j][2])<len(mini[2])):
+                if (len(union_list[j][2])>len(mini[2])):
                     mini=union_list[j]
                     indMin=j
                 j+=1
@@ -109,16 +139,40 @@ class Square():
             i+=1
         return union_list
 
-p1 = Square()
-p5 = Square()
-ar_row = []
-ar_row.append(p1.give_row(0))
-ar_row.append(p1.give_row(1))
-ar_row.append(p1.give_row(2))
-ar_col=[]
-ar_col.append(p5.give_col(0))
-ar_col.append(p5.give_col(1))
-ar_col.append(p5.give_col(2))
-p2 = Square(ar_row, ar_col)
 
-p2.print_croot()
+
+    # def sort_by_union(self,union_list):
+    #     #nie inkrementujesz plus tu chodzi o len setu a nie
+    #     #inicjalizacja zmiennych odpowiedzialnych za pętle
+    #     n = len(union_list)
+    #     i=0
+    #     while(i<=n-1):
+    #         #zadeklarowanie jako mini elementu o inteksie w którym chce wstawić najmniejszą wartość
+    #         mini = union_list[i]
+    #         indMin = i
+    #         j=i+1
+    #         while(j<n):
+    #             #znajduje najmniejszy element z pozostałej listy
+    #             if (len(union_list[j][2])<len(mini[2])):
+    #                 mini=union_list[j]
+    #                 indMin=j
+    #             j+=1
+            
+    #         union_list[indMin] = union_list[i]
+    #         union_list[i] = mini
+    #         i+=1
+    #     return union_list
+
+
+         
+
+app = Sudoku()
+app.sudo_tab[0].create_unbound_place()
+app.sudo_tab[4].create_unbound_place()
+a = app.get_used_in_row(0)
+b = app.get_used_in_column(1)
+lisa = app.sudo_tab[1].create_list_union(a, b)
+lisa = app.sudo_tab[1].sort_by_union(lisa)
+s = []
+app.test_backtrack(lisa, 0, s)
+app.print_full_sudoku()
